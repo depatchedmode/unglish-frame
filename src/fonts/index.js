b189b7ddc2393c08f4ee328197af77c56149776f
ryanbetts <84613835+depatchedmode@users.noreply.github.com>
2024-02-07 12:00:09 -0800

    fonts w better IPA glyph support

--- src/fonts/index.js
+++ src/fonts/index.js
@@ -2,10 +2,17 @@ import { loadFont } from '../../modules/utils';
 
 export default [
     {
-        name: 'Redaction',
-        data: await loadFont('Redaction-Regular.otf'),
+        name: 'Vollkorn',
+        data: await loadFont('EBGaramond-Regular.ttf'),
         previewPath: '/fonts/Redaction-Regular.woff2',
         weight: 400,
         style: 'normal',
+    },
+    {
+        name: 'Vollkorn-Italic',
+        data: await loadFont('EBGaramond-Italic.ttf'),
+        previewPath: '/fonts/Redaction-Regular.woff2',
+        weight: 400,
+        style: 'italic',
     }
 ];
